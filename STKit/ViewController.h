c4318203b85cf5959051bbf0d6de8278316f2cc7
岳克奎 <738816656@qq.com>
2017-05-06 17:05:29 +0800

    1
    1

---
+++ STKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  STKit
+//
+//  Created by 岳克奎 on 17/5/6.
+//  Copyright © 2017年 SlienceTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ STKit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  STKit
+//
+//  Created by 岳克奎 on 17/5/6.
+//  Copyright © 2017年 SlienceTeam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
